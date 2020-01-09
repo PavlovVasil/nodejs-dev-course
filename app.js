b2744200553ba09fdad2ccbfcc4e4a18da954a07
PavlovVasil <vasil@vasilpavlov.com>
2020-01-10 00:14:52 +0200

    Inverse chalk colors

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const chalk = require('chalk');
 const validator = require('validator');
-const getNotes = require('./notes.js');
+// const getNotes = require('./notes.js');
 
 const msg = getNotes();
-const greenMsg = chalk.green('Success');
+const greenMsg = chalk.green.inverse.bold('Success');
 
-console.log(msg);
+// console.log(msg);
 console.log(validator.isEmail('vasil@vasilpavlov.com')); 
 console.log(greenMsg)
-
-// const fs = require('fs');
-// const add = require('./utils.js')
-// fs.appendFileSync('notes.txt', 'I live in Sofia.');
-//const sum = add(4, -2);
-//console.log(sum);
