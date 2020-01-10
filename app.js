37edfa1cd65925e97dcf2463683dfa3d36260d7b
PavlovVasil <vasil@vasilpavlov.com>
2020-01-11 00:26:13 +0200

    Add nodemon dev dependency

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 const chalk = require('chalk');
 const validator = require('validator');
-// const getNotes = require('./notes.js');
-
-const msg = getNotes();
-const greenMsg = chalk.green.inverse.bold('Success');
-
-// console.log(msg);
+const greenMsg = chalk.blue.inverse.bold('Success');
 console.log(validator.isEmail('vasil@vasilpavlov.com')); 
 console.log(greenMsg)
