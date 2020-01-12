8cde6a1dba8fd61bc1d17ebc951083278e02ba0e
PavlovVasil <vasil@vasilpavlov.com>
2020-01-13 00:55:26 +0200

    Add yargs package dependency

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const chalk = require('chalk');
+const yeargs = require('yargs');
 const validator = require('validator');
 
 const command = process.argv[2];
 
 if (command === 'add') {
-    console.log('Adding note!')
+    console.log('Adding note!');
 } else if ( command === 'remove') {
-    console.log('Removing note!')
+    console.log('Removing note!');
 }
