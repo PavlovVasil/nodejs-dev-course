43afe485ad4a8212e6a6ba1df0c15fcd46b12c57
PavlovVasil <vasil@vasilpavlov.com>
2020-01-20 00:49:19 +0200

    Parse json using request

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const url = 'https://api.darksky.net/forecast/7d669a1a064ac43fc67a9abb68e6f0c7/37.8267,-122.4233'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    console.log(`It is currently ${response.body.currently.temperature} degrees out. There is a ${
+        response.body.currently.precipProbability}% of rain.`);
 })
