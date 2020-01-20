a527bb974a374adb59f6f2d72423b78454e4d4fc
PavlovVasil <vasil@vasilpavlov.com>
2020-01-20 22:20:27 +0200

    Get daily summary as well

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/7d669a1a064ac43fc67a9abb68e6f0c7/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/7d669a1a064ac43fc67a9abb68e6f0c7/37.8267,-122.4233?units=si'
 
 request({ url: url, json: true }, (error, response) => {
-    console.log(`It is currently ${response.body.currently.temperature} degrees out. There is a ${
+    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${
         response.body.currently.precipProbability}% of rain.`);
 })
