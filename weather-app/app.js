418f592b8a7a1ff4b774a3883f1192fb74cecc15
PavlovVasil <vasil@vasilpavlov.com>
2020-01-21 22:42:24 +0200

    Get longitude and latitude from geo API

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,3 +6,10 @@ request({ url: url, json: true }, (error, response) => {
     console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${
         response.body.currently.precipProbability}% of rain.`);
 })
+
+const mapUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGF2bG92dmFzaWwiLCJhIjoiY2s1b2J2MWhkMDIyaTNtcDk0NTBqZzFlaiJ9.ogfzl-EOfLJr9iF7v0fAzg&limit=1'
+request({url: mapUrl, json: true}, (error, response) => {
+    const longitude = response.body.features[0].center[0];
+    const latitude = response.body.features[0].center[1];
+    console.log(`Longitude: ${longitude}, latitude: ${latitude}`)
+})
