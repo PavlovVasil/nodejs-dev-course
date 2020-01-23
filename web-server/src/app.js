40b1fe2d71d3fb9fe9c8fdfbea21f33fcb402992
PavlovVasil <vasil@vasilpavlov.com>
2020-01-23 22:59:28 +0200

    Send HTML and JSON as a response

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -11,11 +11,14 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Your weather')
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Sofiq'
+    })
 })
 
 app.listen(3000, () => {
