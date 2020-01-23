db09a4511026ce55273a5ee7febb282a43e039dd
PavlovVasil <vasil@vasilpavlov.com>
2020-01-23 23:12:05 +0200

    Serve a static HTML file

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
-})
+app.use(express.static(publicDirectoryPath));
 
 app.get('/help', (req, res) => {
     res.send('Help page')
