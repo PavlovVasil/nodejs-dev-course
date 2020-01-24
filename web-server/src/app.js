13c86b24e2be81825b420c17b8c2f1511df7c993
PavlovVasil <vasil@vasilpavlov.com>
2020-01-24 20:00:36 +0200

    Convert static files to hbs templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -7,6 +7,26 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(publicDirectoryPath));
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Vasil Pavlov'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Vasil Pavlov'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text'
+    })
+})
+
 app.get('/weather', (req, res) => {
     res.send({
         forecast: 'It is snowing',
