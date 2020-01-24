2590e77c96f1483eb0bcdc30ceafe66c6b715d43
PavlovVasil <vasil@vasilpavlov.com>
2020-01-25 00:43:45 +0200

    Create a footer partial

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -46,6 +46,14 @@ app.get('/weather', (req, res) => {
     })
 })
 
+app.get('/help/*', (req, res) => {
+    res.send('Help article not found')
+})
+
+app.get('*', (req, res) => {
+    res.send('My 404 page')
+})
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000')
 })
