a1c404a4cf3679ec8b364b22e741fb5f2d744fc1
PavlovVasil <vasil@vasilpavlov.com>
2020-01-24 19:48:14 +0200

    Add Handlebars templating engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+app.set('view engine', 'hbs');
 
 app.use(express.static(publicDirectoryPath));
 
