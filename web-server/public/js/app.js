c461389bd42ce38b1ce70e71570c68c5a5b7eee6
PavlovVasil <vasil@vasilpavlov.com>
2020-01-25 17:26:53 +0200

    Add styling and display weather forecast in the browser

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,22 @@
 const form = document.querySelector('form');
 const searchInput = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const address = searchInput.value;
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
     fetch(`http://localhost:3000/weather?address=${address}`)
     .then(response => response.json()
     .then(data => {
         if (data.error) {
-            console.log(data.error)
+            messageOne.textContent = data.error;
         } else {
-            console.log(data.location);
-            console.log(data.forecast);
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast;
         }
 }))
 })
