8845e713e164bf1bc5f7a0245cef77ed11e9117b
PavlovVasil <vasil@vasilpavlov.com>
2020-01-25 15:35:26 +0200

    Create a REST endpoint

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode')
 
 const app = express();
 
@@ -45,10 +47,18 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address'
         })
     }
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Sofia',
-        address: req.query.address
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) { return req.send({ error }) };
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) { return req.send({ error }) }
+            
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
