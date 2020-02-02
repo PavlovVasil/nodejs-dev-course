95c56e27897ac0d994450c775f2f9bae950e88ed
PavlovVasil <vasil@vasilpavlov.com>
2020-02-02 21:42:00 +0200

    Save data from POST request into MongoDB using the User model

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,11 +1,21 @@
-const express = require('express')
+const express = require('express');
+require('./db/mongoose');
+const User = require('./models/user')
 
 const app = express();
 app.use(express.json())
 const port = process.env.PORT || 3000;
 
 app.post('/users', (req, res) => {
-    res.send('Testing')
+    const user = new User(req.body);
+    user
+        .save()
+        .then(() => {
+            res.send(user)
+        })
+        .catch(e => {
+            res.send(e)
+        })
 })
 
 app.listen(port, () => {
