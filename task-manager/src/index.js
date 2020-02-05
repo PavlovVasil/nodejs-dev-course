21289c65ce8aec39ed2b1b60b5f3cca2883ab8a4
PavlovVasil <vasil@vasilpavlov.com>
2020-02-05 22:01:43 +0200

    Send a more specific success response code

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -11,7 +11,7 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
     user
         .save()
-        .then(() => res.send(user))
+        .then(() => res.status(201).send(user))
         .catch(e => res.status(400).send(e))
 });
 
@@ -19,7 +19,7 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
     task
         .save()
-        .then(() => res.send(task))
+        .then(() => res.status(201).send(task))
         .catch(e => res.status(400).send(e))
 });
 
