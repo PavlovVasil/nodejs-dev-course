bb6361bd0d4f0be6853c288622d93ec242f5b3ba
PavlovVasil <vasil@vasilpavlov.com>
2020-02-09 23:03:18 +0200

    Use async await instead of promise chaining

--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -1,7 +1,17 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5e2f53ba1ea3ef197417875a')
-    .then(task => Task.countDocuments({completed: false}))
+// Task.findByIdAndDelete('5e2f53ba1ea3ef197417875a')
+//     .then(task => Task.countDocuments({completed: false}))
+//     .then(result => console.log(result))
+//     .catch(e => console.log(e));
+
+const deleteTaskAndCound = async id => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count
+};
+
+deleteTaskAndCound('5e2f53ba1ea3ef197417875a')
     .then(result => console.log(result))
-    .catch(e => console.log(e))
+    .catch(e => console.log(e));
