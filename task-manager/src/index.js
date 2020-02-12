d94a497aa11d9a3ab0b5591faff83b100c8cd839
PavlovVasil <vasil@vasilpavlov.com>
2020-02-12 19:57:01 +0200

    Add an http patch handler and validation for tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -90,6 +90,26 @@ app.get('/tasks/:id', async (req, res) => {
     } catch (e) {
         res.status(500).send()
     }
+});
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['description', 'completed'];
+    const isOperationValid = updates.every(update => allowedUpdates.includes(update));
+    
+    if(!isOperationValid) {
+        req.status(400).send({ error: 'Invalid updates!'})
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id,
+            req.body, {new: true, runValidators: true });
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 app.listen(port, () => {
