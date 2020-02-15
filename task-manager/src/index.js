d2b7b46c1beeb0e99a6c35563b5959bba72c6a95
PavlovVasil <vasil@vasilpavlov.com>
2020-02-15 20:37:44 +0200

    Test bcrypt hash and compare functions

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -11,4 +11,16 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
-})
+});
+
+const bcrypt = require('bcrypt');
+
+ (async () => {
+    const password = 'Test1234';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('Test1234', hashedPassword);
+    console.log(isMatch);
+})();
