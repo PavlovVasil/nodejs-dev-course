f354c1521afca0faa83c3646836bd249654d55fb
PavlovVasil <vasil@vasilpavlov.com>
2020-02-16 20:44:16 +0200

    Add a static findByCredentials method to check the hashed user password

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         trim: true,
         lowercase: true,
+        unique: true,
         validate(value) {
             if(!validator.isEmail(value)) {
                 throw new Error ('Email is invalid')
@@ -40,6 +41,19 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+    return user;
+}
+
+//Hash the plain password before saving in the DB
 userSchema.pre('save', async function (next) {
     const user = this;
     if(user.isModified('password')) {
