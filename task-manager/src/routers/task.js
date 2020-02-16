963369a01e88994117879a78c4de77be5af863cc
PavlovVasil <vasil@vasilpavlov.com>
2020-02-16 20:22:36 +0200

    Refactor the task router for consistency

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -43,8 +43,9 @@ router.patch('/tasks/:id', async (req, res) => {
         req.status(400).send({ error: 'Invalid updates!'});
     }
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id,
-            req.body, {new: true, runValidators: true });
+        const task = await Task.findById(req.params.id);
+            updates.forEacg(update => task[update] = req.body[update]);
+            await task.save();
         if (!task) {
             return res.status(404).send();
         }
