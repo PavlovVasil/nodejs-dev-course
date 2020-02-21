de896880e59f45e3e0583c2047a49eeb4190b859
PavlovVasil <vasil@vasilpavlov.com>
2020-02-21 20:22:28 +0200

    Use middleware in the user router

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -13,9 +13,9 @@ const app = express();
 //     }
 // });
 
-app.use((req, res, next) => {
-    res.status(503).send('Site is currently down. Check back soon!')
-})
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is currently down. Check back soon!')
+// })
 
 app.use(express.json());
 app.use(userRouter);
