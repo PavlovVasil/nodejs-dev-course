326a4115567459a1843c7500f89a57fd3e14de85
PavlovVasil <vasil@vasilpavlov.com>
2020-02-22 23:11:04 +0200

    Add sorting to the tasks end - point

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -19,9 +19,15 @@ router.post('/tasks', auth, async (req, res) => {
 router.get('/tasks', auth, async (req, res) => {
     try {
         const match = {};
+        const sort = {};
 
         if( req.query.completed) {
             match.completed = req.query.completed === 'true'
+        };
+
+        if(req.query.sortBy) {
+            const parts = req.query.sortBy.split(':');
+            sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
         }
 
         await req.user.populate({
@@ -29,7 +35,8 @@ router.get('/tasks', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit, 10),
-                skip: parseInt(req.query.skip, 10)
+                skip: parseInt(req.query.skip, 10),
+                sort
             }
         }).execPopulate();
         res.send(req.user.tasks);
