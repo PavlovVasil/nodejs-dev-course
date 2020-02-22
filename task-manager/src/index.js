c0c983ef973b211deb9bb81e18d0e2add64b7975
PavlovVasil <vasil@vasilpavlov.com>
2020-02-22 23:16:33 +0200

    Add multer as a dependency, remove unnecessary code

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,17 +5,7 @@ const taskRouter = require('./routers/task');
 
 const app = express();
 
-// app.use((req, res, next) => {
-//     if(req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next();
-//     }
-// });
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
 
 app.use(express.json());
 app.use(userRouter);
@@ -25,12 +15,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// (async () => {
-//     const user = await User.findById('5e51354688569b25248c3494')
-//     await user.populate('tasks').execPopulate();
-//     console.log(user.tasks)
-// })()
