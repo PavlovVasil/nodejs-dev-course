41dbb6479086afa4053234bb726eebf49c3f9857
PavlovVasil <vasil@vasilpavlov.com>
2020-02-22 16:00:39 +0200

    Use auth for user update end-point

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -49,7 +49,7 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 });
 
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
     const isOperationValid = updates.every(update => allowedUpdates.includes(update));
@@ -58,13 +58,9 @@ router.patch('/users/:id', async (req, res) => {
         req.status(400).send({ error: 'Invalid updates!'});
     }
     try {
-        const user = await User.findById(req.params.id);
-        updates.forEach(update => user[update] = req.body[update]);
-        await user.save();
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
+        updates.forEach(update => req.user[update] = req.body[update]);
+        await req.user.save();
+        res.send(req.user);
     } catch (e) {
         res.status(500).send(e);
     }
