37c4ad6e4d67f9b3199c4ad544abc2cf362f5ba1
PavlovVasil <vasil@vasilpavlov.com>
2020-02-23 22:44:29 +0200

    Test multer and save files uploaded to the server

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,6 +5,15 @@ const taskRouter = require('./routers/task');
 
 const app = express();
 
+const multer = require('multer');
+
+const upload = multer({
+    dest: 'images'
+});
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
 
 
 app.use(express.json());
@@ -15,3 +24,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
+
+
