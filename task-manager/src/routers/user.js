3378cd504cf32e238cdf345a32c08f17b81e0d3e
PavlovVasil <vasil@vasilpavlov.com>
2020-02-27 09:15:24 +0200

    Convert all avatars to png and resize them before saving them in the DB

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const multer = require('multer');
+const sharp = require('sharp');
 
 
 router.post('/users', async (req, res) => {
@@ -89,7 +90,15 @@ const upload = multer({
 });
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer;
+    const buffer = await sharp(req.file.buffer)
+        .resize({
+            width: 250,
+            height: 250
+        })
+        .png()
+        .toBuffer();
+    
+    req.user.avatar = buffer;
     await req.user.save();
     res.send()
 }, (error, req, res, next) => {
@@ -108,7 +117,7 @@ router.get('/users/:id/avatar', async (req, res) => {
         if (!user || !user.avatar) {
             throw new Error ();
         }
-        res.set('Content-Type', 'image/jpg').send(user.avatar);
+        res.set('Content-Type', 'image/png').send(user.avatar);
     } catch (e) {
         res.status(400).send();
     }
