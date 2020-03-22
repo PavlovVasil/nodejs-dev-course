d9d6a4d8c70d9620a35af093e758b0f54faa6324
PavlovVasil <vasil@vasilpavlov.com>
2020-03-22 14:50:46 +0200

    Allow users to share their location and show it on google maps

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -9,3 +9,13 @@ document.querySelector('#message-form').addEventListener('submit', e => {
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#send-location').addEventListener('click', (e) => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition(({coords: {longitude, latitude}}) => {
+        socket.emit('sendLocation', {latitude, longitude})
+    })
+})
