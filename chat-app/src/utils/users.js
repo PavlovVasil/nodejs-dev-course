ffb7bfe8d97883cd49cd3a0a1b26f54e7a8fb381
PavlovVasil <vasil@vasilpavlov.com>
2020-04-12 22:35:21 +0300

    Add methods to get a user and all users in a room

--- chat-app/src/utils/users.js
+++ chat-app/src/utils/users.js
@@ -36,4 +36,8 @@ const removeUser = (id) => {
     if(index !== -1) {
         return users.splice(index, 1)[0];
     }
+};
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
 }
