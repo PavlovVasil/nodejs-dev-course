480ee6ac774d6631b7fe0abc11268a18e224d48f
PavlovVasil <vasil@vasilpavlov.com>
2020-04-16 22:19:28 +0300

    Add users list to the page

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -10,6 +10,7 @@ const $messages = document.querySelector('#messages');
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
 //Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true});
@@ -74,6 +75,9 @@ socket.emit('join', { username, room }, (error) => {
 })
 
 socket.on('roomData', ({room, users}) => {
-    console.log(room);
-    console.log(users)
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    });
+    document.querySelector("#sidebar").innerHTML = html;
 })
